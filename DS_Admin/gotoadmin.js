9dc7611fa4f7422dab96531845b0f4534e6a104f
Michael Garcia <33134352+mnitservices23@users.noreply.github.com>
2018-07-04 16:45:46 -0600

    Add files via upload

---
+++ DS_Admin/gotoadmin.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('gobutton');
+  
+
+    chrome.tabs.getSelected(null, function(tab) {
+        checkPageButton.addEventListener('click', function() {    
+            var new_url = tab.url.split('/');
+            new_url = new_url[0]+'//'+new_url[2]+'/admin';
+            chrome.tabs.update({ url:  new_url});
+            console.log(new_url);
+         }, false);
+    });
+
+}, false);   
+
+
+    
