61fcabe49b6386872b49da72bb6f72a11873367d
geekmors <39037255+geekmors@users.noreply.github.com>
2018-07-04 18:21:17 -0600

    added open new tab functionality
    added line to create new tab with new link when button "admin" is clicked

--- DS_Admin/gotoadmin.js
+++ DS_Admin/gotoadmin.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
         checkPageButton.addEventListener('click', function() {    
             var new_url = tab.url.split('/');
             new_url = new_url[0]+'//'+new_url[2]+'/admin';
-            chrome.tabs.update({ url:  new_url});
-            console.log(new_url);
+            chrome.tabs.create({ url: new_url });  
+          //chrome.tabs.update({ url:  new_url});
+            //console.log(new_url);
          }, false);
     });
 
