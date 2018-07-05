b7dab18f2704de28e237e51f608c01a57048fae9
geekmors <39037255+geekmors@users.noreply.github.com>
2018-07-05 16:54:19 -0600

    Update gotoadmin.js

--- DS_Admin/gotoadmin.js
+++ DS_Admin/gotoadmin.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('gobutton');
-  
+    
+    function checkSite(){
+        
+        var hold = document.getElementsByClassName("footer-DS");
+        
+        return (hold.length > 0)?true:false;
+        
+    }
+    var is_our_site;
+      chrome.tabs.executeScript({
+        code: '(' + checkSite + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+          is_our_site = results[0];
+    });
 
     chrome.tabs.getSelected(null, function(tab) {
-        checkPageButton.addEventListener('click', function() {    
-            var new_url = tab.url.split('/');
-            new_url = new_url[0]+'//'+new_url[2]+'/admin';
-            chrome.tabs.create({ url: new_url });  
-          //chrome.tabs.update({ url:  new_url});
-            //console.log(new_url);
+        checkPageButton.addEventListener('click', function() { 
+            if(is_our_site){
+                var new_url = tab.url.split('/');
+                new_url = new_url[0]+'//'+new_url[2]+'/admin';
+                chrome.tabs.create({ url: new_url }); 
+            }
+            
          }, false);
     });
 
